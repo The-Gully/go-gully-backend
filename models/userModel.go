efb091fdd8b50ff281881ebd5c8a81168691dbdb
Astrasv <sudharsan.vanamali@gmail.com>
2026-03-24 19:55:00 +0530

    User profile avatar OAuth

--- models/userModel.go
+++ models/userModel.go
@@ -4,13 +4,13 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email      string `gorm:"unique"`
-	Password   string
-	ProviderID string `gorm:"index"`
-	Role       string `gorm:"default:user"`
-	Provider   string
-	AvatarURL  string
-	Name       string
+	Email      string `gorm:"unique" json:"email"`
+	Password   string `json:"-"`
+	ProviderID string `gorm:"index" json:"provider_id"`
+	Role       string `gorm:"default:user" json:"role"`
+	Provider   string `json:"provider"`
+	AvatarURL  string `json:"avatar_url"`
+	Name       string `json:"name"`
 }
 
 type Role string
